3c7aa7c66e7db5f88a287613e83243495e78c285
brunostracke <brunostracke@gmail.com>
2023-08-17 19:42:41 -0300

    add material-ui design

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,33 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
-import axios from 'axios';
-import React from 'react';
+import styles from "../styles/Home.module.css";
+import axios from "axios";
+import React from "react";
+import { DataGrid } from "@mui/x-data-grid";
 
-const baseApiUrl = process.env.NEXT_PUBLIC_GROOVE_API
+const baseApiUrl = process.env.NEXT_PUBLIC_GROOVE_API;
 
 export default function Home() {
   const [codes, setCodes] = React.useState([]);
-  const [isDataFetched, setIsDataFetched] = React.useState(false)
-  
+  const [isDataFetched, setIsDataFetched] = React.useState(false);
 
-  React.useEffect( () => {
-    console.log(baseApiUrl)
-    axios.get(baseApiUrl)
-    .then(data => {
-      console.log(data.data)
-      setCodes(data.data)
-      setIsDataFetched(true)
-    })
+  const collumns = [
+    { field: "code", headerName: "CODE" },
+    { field: "quantity", headerName: "QUANTIDADE" },
+  ];
 
-  }, [])
+  React.useEffect(() => {
+    axios.get(baseApiUrl).then((data) => {
+      setCodes(data.data);
+      setIsDataFetched(true);
+    });
+  }, []);
 
-  if(codes.length === 0) return <div>loading</div>
+  if (codes.length === 0) return <div>Loading...</div>;
 
   return (
     <div className={styles.container}>
-      {isDataFetched && <div> {Object.keys(codes).map(code => <div key={code}>{code}: {codes[code]}</div>)}</div>}
+      {isDataFetched && (
+        <DataGrid columns={collumns} rows={codes} hideFooterPagination autoHeight />
+      )}
     </div>
-  )
+  );
 }
